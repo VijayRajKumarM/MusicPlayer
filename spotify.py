52e085359a0b9286f9f9b524750bc49683782ef3
VijayRajKumarM <108127448+VijayRajKumarM@users.noreply.github.com>
2024-01-17 23:57:03 +0530

    Add files via upload
    I hereby attach the flask server file for the spotify api

--- spotify.py
+++ spotify.py
@@ -4,45 +4,13 @@ import webbrowser
 import pymongo
 from flask import Flask, request, jsonify 
 app = Flask(__name__)
-@app.route('/', methods=['POST','GET'])
-def users_login():
-    '''
-    myclient = pymongo.MongoClient("mongodb://localhost:27017/?directConnection=true")
-    mydb = myclient["musicplayer"]
-    mycol = mydb["user1"]
-    print('completed')
-    print('completed1')
-    mycol.insert_one(
-        {
-            'name' : 'vijayrajkumar',
-            'password' : 'vijayrajkumar@gmail.com',
-            'playlist' : {
-                'gym' : ['uewyteuifh8e4t4eww','urigfyw94yryhow']
-            }
-        }
-    )
-    result = mycol.find_one({'name': 'vijayrajkumar'})
-    if result:
-        gym_playlist = result['playlist']['gym'][0] 
-        print(gym_playlist)
-    return "hello"
-    '''
-    # Flask server code
-    if request.method == 'POST':
-        data = request.json
-        name = data.get('name')
-        password = data.get('password')
-
-        # Your login logic here using name and password
-
-        return jsonify({'message': 'Login successful', 'name': name})
-    else:
-        return "Hello from Flask!"
-@app.route('/searchsong', methods=['POST','GET'])
-def searchsong():
-   if request.method == 'POST':
-       data = request.json
-       song = data.get('name')
+@app.route('/searchsong/<string:song>', methods=['POST','GET'])
+def searchsong(song):
+   #if request.method == 'POST':
+   if(2+2==4):
+       #data = request.json
+       #song = data.get('songname')
+       #song = "jailer"
        cnt = 0
        username = '3156iwh2shkvlohsrcn7xv74rxoy'
        clientID = '04924240c02b4ceca8e9953b2910f5cd'
@@ -57,30 +25,41 @@ def searchsong():
        print("yes")
        print(json.dumps(user_name, sort_keys=True, indent=4)) 
        while True: 
-        print("Welcome to the project, " + user_name['display_name']) 
-        print("0 - Exit the console") 
-        print("1 - Search for a Song") 
-        user_input = int(input("Enter Your Choice: ")) 
+        #print("Welcome to the project, " + user_name['display_name']) 
+        #print("0 - Exit the console") 
+        #print("1 - Search for a Song") 
+        user_input = 1
         if user_input == 1: 
-            search_song = input("Enter the song name: ") 
+            #search_song = input("Enter the song name: ") 
+            search_song = song 
             results = spotifyObject.search(search_song, 1, 0, "track") 
             songs_dict = results['tracks'] 
             song_items = songs_dict['items'] 
+            print("\n\n\n")
             cnt = 0
             for i in songs_dict.values():
                 cnt+=1
                 if(cnt==2):
                     break 
                 else:
-                    print(i)
+                    #print(i)
+                    pass
             cnt = 0
             for i in song_items:
                 cnt+=1
                 if(cnt==2):
                     break 
                 else:
-                    print(i)
+                    #print(i)
+                    pass
+            print('\n\n\n')
             song = song_items[0]['external_urls']['spotify'] 
+            print("\n\n\n")
+            print(song_items[0]['album'])
+            artist = song_items[0]['album']['artists'][0]['name']
+            album = song_items[0]['album']['name']
+            print("album=",song_items[0]['album']['name'])
+            print("artist=",artist)
             songurl = song_items[0]['preview_url']
             song_id = song_items[0]['id']
             print("\n\nsong url less=",song)
@@ -93,7 +72,7 @@ def searchsong():
             break
         else: 
             print("Please enter valid user-input.")
-        return jsonify({'trackid':songurl})
+        return jsonify({'trackid':songurl,'album':album,'artist':artist})
    else:
        return "Hello function 21"
 if __name__ == '__main__':
